81d4d0c3829934648a0b4eafb8bfdfea85f060b5
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-10-14 14:45:59 -0400

    j

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import hero from "../../public/hero.jpg"
 
 export default function Home() {
   function cloudinaryLoader({ src, width, quality }) {
@@ -18,14 +19,15 @@ export default function Home() {
       I rende
       <Image
         src={
-          "https://newburypublic.s3.amazonaws.com/website/page-home-hero.jpg"
+         hero
         }
         fill={true}
         sizes="100vw"
         quality={100}
         alt="Apartments Cover Image"
         style={{ objectFit: "cover" }}
-        priority={true}
+        placeholder="blur"
+        
       />
     </div>
   );
