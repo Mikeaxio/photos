55fb800d6b330cb47b835b4c4bbfe3c17e0840d7
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-10-13 21:58:20 -0400

    add"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["newburypublic.s3.amazonaws.com"],
+        domains: ["newburypublic.s3.amazonaws.com", "newbury-887694351.imgix.net"], 
+        minimumCacheTTL: 600,
       },
 }
 
