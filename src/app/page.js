5347b703707fb35dc39b30a3fe4ab9661f819023
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-10-13 21:24:17 -0400

    more

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  function imgixLoader({ src, width, quality }) {
+    const url = new URL(`https://example.com${src}`)
+    const params = url.searchParams
+    params.set('auto', params.getAll('auto').join(',') || 'format')
+    params.set('fit', params.get('fit') || 'max')
+    params.set('w', params.get('w') || width.toString())
+    params.set('q', (quality || 50).toString())
+    return url.href
+  }
+
   return (
     <div>
       I rendered
       <Image
         src={
-          "https://newburypublic.s3.amazonaws.com/website/delete-min.jpg"
+          "/delete.jpg"
         }
         fill
         sizes="100vw"
