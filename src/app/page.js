197292ebd6e768aafd824d5b6568656abce834f0
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-10-15 13:14:16 -0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,19 @@
 "use client";
+import { CldImage } from "next-cloudinary";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  function cloudinaryLoader({ src, width, quality }) {
-    const params = [
-      "f_auto",
-      "c_limit",
-      `w_${width}`,
-      `q_${quality || "auto"}`,
-    ];
-    return `https://newburypublic.s3.amazonaws.com/website/page-contact-cover.jpg`;
-  }
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       I rende
-      <Image
-        src={
-          "https://newburypublic.s3.amazonaws.com/website/del.jpg"
-        }
-        width={8660}
-        height={5773}
-        quality={100}
-        sizes={8660}
-        alt="Apartments Cover Image"
-        style={{ objectFit: "cover" }}
+      <CldImage
+      width={5000}
+      height={3333}
+      src="page-contact-cover_2_isvft4"
+      alt="Apartments Cover Image"
+
       />
     </div>
   );
