38436d2dc575c73fa58bbf550f4b7e764bab5a9f
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-10-16 15:03:50 -0400

    m

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,21 @@
-"use client";
-import { CldImage } from "next-cloudinary";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 
 
 export default function Home() {
-  function imgixLoader({ src, width, quality }) {
-    const url = new URL(`https://newbury-887694351.imgix.net${src}`)
-    const params = url.searchParams
-    params.set('auto', params.getAll('auto').join(',') || 'format')
-    params.set('fit', params.get('fit') || 'max')
-    params.set('w', params.get('w') || width.toString())
-    params.set('q', (quality || 70).toString())
-    return url.href
-  }
+
   return (
 
       <div >
         I rende
         <Image
-        loader={imgixLoader}
           fill={true}
-          src="/website/page-home-hero.jpg"
+          src="https://newburypublic.s3.amazonaws.com/generics/iStock-145155089.jpg"
           alt="Apartments Cover Image"
           style={{ objectFit: "cover" }}
+          
         />
       </div>
 
